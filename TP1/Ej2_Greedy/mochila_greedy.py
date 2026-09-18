25e732acd0128e8d63f69a0c7a114c69ffaf9a4e
LucasLGigirey <llaroccag@fi.uba.ar>
2026-09-18 14:31:46 -0300

    Agrego funcionalidad de mochila greedy, con garantia de calidad 1/2, ademas modifico import

--- TP1/Ej2_Greedy/mochila_greedy.py
+++ TP1/Ej2_Greedy/mochila_greedy.py
@@ -1,4 +1,9 @@
-import crear_mochila
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from crear_mochila import crear_mochila
 
 def lectura_archivo(nombre):
 
@@ -19,3 +24,36 @@ def lectura_archivo(nombre):
 
     return capacidad, elementos_ordenados
 
+def mochila_greedy(capacidad, elementos):
+
+    peso_actual = 0
+    beneficio_total = 0
+
+    for peso, beneficio in elementos:
+        if peso_actual + peso <= capacidad:
+            peso_actual += peso
+            beneficio_total += beneficio
+
+    mejor_elemento = 0
+
+    # Garantida de calidad 1/2
+
+    for peso, beneficio in elementos:
+        if peso <= capacidad:
+            if beneficio > mejor_elemento:
+                mejor_elemento = beneficio
+
+    return max(beneficio_total, mejor_elemento)
+
+def main():
+
+    nombre_archivo = "mochila1000.txt"
+
+    capacidad, elementos = lectura_archivo(nombre_archivo)
+    beneficio_maximo = mochila_greedy(capacidad, elementos)
+
+    print(f"Capacidad de la mochila: {capacidad}")
+    print(f"Beneficio máximo obtenido: {beneficio_maximo}")
+
+
+main()
